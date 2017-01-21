1512fdd6524529e1209117f335d9ca206f7446df
Tyler Springer <mspringer@smu.edu>
2017-01-20 17:41:53 -0800

    Added all preliminary files and license information. Will add many updated and documentation soon

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # enc
 Python Code Base Encryption
+
+Many many updates coming...as well as instructions, examples, etc...stay tuned
--- run.py
+++ run.py
@@ -1,3 +1,19 @@
+#     enc - Run Module - A free utility for encrypting Python code bases.
+#     Copyright (C) 2017 M. Tyler Springer
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from collections import OrderedDict
